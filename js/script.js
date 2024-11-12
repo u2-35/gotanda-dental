bd69b42dee46b1ad55b3d567b492c78b256cf833
k-ono63 <ok0603.maple@gmail.com>
2024-11-13 01:52:14 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 $(function () {
 	$('.menu-btn').click(function () {
-		$('.nav-container').toggleClass('open');
-        $(this).find('i').toggleClass('fa-bars fa-xmark');
-	});
+        if($('.nav-container').hasClass('open')) {
+            $('.nav-container').removeClass('open');
+            $('.menu-btn').find('i').removeClass('fa-xmark');
+            $('.menu-btn').find('i').addClass('fa-bars');
+        }else{
+            $('.nav-container').addClass('open');
+            $('.menu-btn').find('i').addClass('fa-xmark');
+            $('.menu-btn').find('i').removeClass('fa-bars');
+        }
+        if($('.nav-container').hasClass('open')) {
+            $('main').click(function(){
+                $('.nav-container').removeClass('open');
+                $('.menu-btn').find('i').removeClass('fa-xmark');
+                $('.menu-btn').find('i').addClass('fa-bars');
+            });
+        }
+    });
 });
